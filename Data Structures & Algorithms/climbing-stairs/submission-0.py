0dd8f5c6879a91a43bae1a9938ef23f8293cbf4a
Gullapalli Chetan <chetangips@gmail.com>
2026-08-09 22:17:59 +0800

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<3:
+            return n
+        arr = [0]*n
+        arr[0], arr[1] = 1, 2
+        for i in range(2, n):
+            arr[i]=arr[i-1]+arr[i-2]
+        return arr[-1]
